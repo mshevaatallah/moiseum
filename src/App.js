91f6e018fd3802551514a8870e98c5c1e8c58563
sxhvxa <muhammadshax07@gmail.com>
2023-02-08 20:33:09 +0700

    add aca

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>halo semuanya</p>
+        <p>halo semunya</p>
         <a
           className="App-link"
           href="https://reactjs.org"
