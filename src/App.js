1a8ca8453d5c9ceaa0d9bde221d025084e13abe2
sxhvxa <muhammadshax07@gmail.com>
2023-02-09 11:16:12 +0700

    add structure

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>halo semuanya gw rasya</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React bareng Aca
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
