14175c34bfc139f3d9a7420631565ee5fd0dcbd6
sxhvxa <muhammadshax07@gmail.com>
2023-02-09 11:19:21 +0700

    add pages

--- src/App.js
+++ src/App.js
@@ -4,18 +4,11 @@ import Home from "./pages/Home";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>halo semuanya gw rasya</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React bareng Aca
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
