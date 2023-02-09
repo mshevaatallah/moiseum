e3afb600744eecfaec12470909f8e1556fc79ee1
sxhvxa <muhammadshax07@gmail.com>
2023-02-09 11:21:20 +0700

    add

--- src/App.js
+++ src/App.js
@@ -4,18 +4,11 @@ import Home from "./pages/Home";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>halo</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
