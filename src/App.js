f1ff116b1000154259955751bf71c767c5a9ee5b
xanaqwert <95111419+xanaqwert@users.noreply.github.com>
2023-02-10 10:21:10 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,254 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import Pages2 from "./pages/Pages2";
-import Pages3 from "./pages/Pages3";
-import Pages4 from "./pages/Pages4";
-import Pages5 from "./pages/Pages5";
-import Pages6 from "./pages/Pages6";
+import Spline from "@splinetool/react-spline";
+import styled from "styled-components";
+import "./styles.css";
 
-function App() {
+import Logo from "./images/logo.svg";
+import IconTwitter from "./images/icon-twitter.svg";
+import IconYouTube from "./images/icon-youtube.svg";
+import arrow from "./images/arrow.png"
+
+export default function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/sejarah1" element={<Pages2 />} />
-          <Route path="/sejarah2" element={<Pages3 />} />
-          <Route path="/sejarah3" element={<Pages4 />} />
-          <Route path="/sejarah4" element={<Pages5 />} />
-          <Route path="/sejarah5" element={<Pages6 />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+/* Main Container */
+    <Wrapper>
+      <Spline className="spline" scene="https://prod.spline.design/GF6Cxoiu-CRodU8Z/scene.splinecode" />
+      <Social>
+        <div />
+        <img src={IconTwitter} alt="Twitter" />
+        <img src={IconYouTube} alt="YouTube" />
+      </Social>
+      <Content>
+        <Menu>
+          <li>
+            <img src={Logo} alt="title" />
+          </li>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/">Virtual Museum</a>
+          </li>
+          <li>
+            <a href="/">About</a>
+          </li>
+          <li>
+            <a href="/">Audio Guide</a>
+          </li>
+          <li>
+            <button>Get Started</button>
+          </li>
+        </Menu>
+        <h1>Modern Indonesia Museum</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at
+          risus eget ligula rhoncus aliquet nec id erat. Praesent malesuada
+          vulputate consectetur. Aenean nec porta quam.
+        </p>
+        <button>
+          <img className="arrow-icon" src={arrow} alt="" /> Start free trial
+        </button>
+      </Content>
+    </Wrapper>
   );
 }
 
-export default App;
+/* BAGIAN CSS */
+/*CSS nya make styled component, mirip kayak sass/scss*/
+
+const Wrapper = styled.div`
+  font-family: "Spline Sans", sans-serif;
+  color: white;
+  font-size: 16px;
+  margin: 0 auto;
+  position: relative;
+  height: 100%;
+  overflow-x: hidden;
+
+  .spline {
+    position: absolute;
+    margin: 0;
+    top: 0;
+    right: 0;
+    width: 1200px;
+    height: 1000px;
+
+    @media (max-width: 1024px) {
+      transform: scale(0.8) translateX(200px);
+      transform-origin: top;
+    }
+    @media (max-width: 800px) {
+      transform: scale(0.7) translateX(600px);
+    }
+    @media (max-width: 600px) {
+      transform: scale(0.5) translateX(-100px);
+      right: auto;
+      left: 50%;
+      margin-left: -600px;
+    }
+    @media (max-width: 375px) {
+      transform: scale(0.45) translateX(-50px);
+    }
+  }
+`;
+
+const Content = styled.div`
+  position: absolute;
+  top: 30px;
+  width: 100%;
+  padding-bottom: 100px;
+  pointer-events: none;
+
+  display: flex;
+  flex-direction: column;
+  gap: 80px;
+
+  @media (max-width: 1024px) {
+    gap: 40px;
+  }
+
+  .arrow-icon {
+  width: 25px;
+  height: 25px;
+}
+
+  h1 {
+    font-weight: bold;
+    font-family: "Spline Sans Mono", monospace;
+    font-size: 70px;
+    margin: 0;
+    max-width: 500px;
+    pointer-events: auto;
+    text-shadow: 0 10px 30px rgba(0, 0, 0, 0.5);
+
+    @media (max-width: 1024px) {
+      font-size: 60px;
+      max-width: 400px;
+    }
+    @media (max-width: 800px) {
+      font-size: 40px;
+      max-width: 300px;
+    }
+    @media (max-width: 600px) {
+      padding-top: 250px;
+    }
+  }
+
+  p {
+    font-weight: normal;
+    line-height: 150%;
+    max-width: 380px;
+    pointer-events: auto;
+    text-shadow: 0 10px 30px rgba(0, 0, 0, 0.5);
+  }
+
+  button {
+    background: rgba(0, 0, 0, 0.2);
+    border: 0px;
+    font-size: 16px;
+    padding: 12px 30px;
+    border-radius: 14px;
+    color: white;
+    border: 1px solid rgba(255, 255, 255, 0.1);
+    max-width: 280px;
+    backdrop-filter: blur(20px);
+    font-weight: 600;
+    box-shadow: 0 20px 20px rgba(0, 0, 0, 0.2);
+    transition: 1s;
+    cursor: pointer;
+    pointer-events: auto;
+
+    display: flex;
+    gap: 12px;
+    justify-content: center;
+    align-items: center;
+
+    :hover {
+      border: 1px solid rgba(255, 255, 255, 0.8);
+      transform: translateY(-3px);
+    }
+  }
+
+  h1,
+  p,
+  button {
+    margin: 0 30px 0 100px;
+
+    @media (max-width: 1024px) {
+      margin: 0 30px;
+    }
+  }
+`;
+
+const Menu = styled.ul`
+  display: flex;
+  gap: 30px;
+  align-items: center;
+  margin: 0 30px 0 100px;
+  padding: 0;
+  pointer-events: auto;
+
+  @media (max-width: 1024px) {
+    margin: 0 30px;
+  }
+
+  li {
+    list-style: none;
+    margin: 0;
+
+    a {
+      text-decoration: none;
+      color: white;
+      padding: 8px 20px;
+      border-radius: 14px;
+      border: 1px solid rgba(255, 255, 255, 0);
+      transition: 1s;
+
+      :hover {
+        border: 1px solid rgba(255, 255, 255, 0.2);
+      }
+    }
+  }
+
+  button {
+    margin: 0;
+    width: auto;
+    background: rgba(31, 66, 250, 0.2);
+    border: 1px solid rgba(255, 255, 255, 0.4);
+  }
+
+  @media (max-width: 800px) {
+    justify-content: space-between;
+    li:nth-child(2),
+    li:nth-child(3),
+    li:nth-child(4),
+    li:nth-child(5) {
+      display: none;
+    }
+  }
+`;
+
+const Social = styled.div`
+  position: absolute;
+  top: 150px;
+  left: 30px;
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+  align-items: center;
+
+  @media (max-width: 1024px) {
+    display: none;
+  }
+
+  div {
+    width: 1px;
+    height: 500px;
+    background: linear-gradient(
+      180deg,
+      #08b6f9 0%,
+      #6c56ef 33.57%,
+      #1306dd 65.86%,
+      #aa0eb2 100%
+    );
+  }
+`;
