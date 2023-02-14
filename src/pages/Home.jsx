3c5db6b2d43646c3069a448b786844d1aa035797
sxhvxa <muhammadshax07@gmail.com>
2023-02-14 10:58:03 +0700

    add card fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,29 +6,29 @@ import "aos/dist/aos.css";
 import Footer from "../components/Footer";
 
 function Home() {
-  // useEffect(() => {
-  //   AOS.init();
-  // }, []);
-  // const [isLoading, setLoading] = useState(true);
-  // function someRequest() {
-  //   //Simulates a request; makes a "promise" that'll run for 2.5 seconds
-  //   return new Promise((resolve) => setTimeout(() => resolve(), 2500));
-  // }
+  useEffect(() => {
+    AOS.init();
+  }, []);
+  const [isLoading, setLoading] = useState(true);
+  function someRequest() {
+    //Simulates a request; makes a "promise" that'll run for 2.5 seconds
+    return new Promise((resolve) => setTimeout(() => resolve(), 2500));
+  }
 
-  // useEffect(() => {
-  //   someRequest().then(() => {
-  //     const loaderElement = document.querySelector(".loader-container");
-  //     if (loaderElement) {
-  //       loaderElement.remove();
-  //       setLoading(!isLoading);
-  //     }
-  //   });
-  // });
+  useEffect(() => {
+    someRequest().then(() => {
+      const loaderElement = document.querySelector(".loader-container");
+      if (loaderElement) {
+        loaderElement.remove();
+        setLoading(!isLoading);
+      }
+    });
+  });
 
-  // if (isLoading) {
-  //   //
-  //   return null;
-  // }
+  if (isLoading) {
+    //
+    return null;
+  }
 
   return (
     <div>
